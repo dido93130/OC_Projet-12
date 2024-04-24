17ba6da93416494a266b26b6b86f8710651f0a9b
Eddy Capirchio <dido93430@hotmail.fr>
2024-04-24 14:50:02 +0200

    Projet12 v2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "./",
+  plugins: [ react()],
+  base: "/OC_Projet-12/",
+  server: {
+    port: 3001,
+  }, 
+});
